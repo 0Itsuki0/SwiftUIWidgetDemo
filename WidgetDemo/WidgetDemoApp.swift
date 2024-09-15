4c3d327fc09e5e1f2e7b480aecac5d971a88282e
itsuki <itsuki.enjyoy@gmail.com>
2024-09-16 08:31:53 +0900

    Initial Commit

---
+++ WidgetDemo/WidgetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetDemoApp.swift
+//  WidgetDemo
+//
+//  Created by Itsuki on 2024/09/16.
+//
+
+import SwiftUI
+
+@main
+struct WidgetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
